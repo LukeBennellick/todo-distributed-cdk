0eeee90aa823b722116f630908ac921b155e230c
Luke Bennellick <lukebennellick18@gmail.com>
2025-02-06 15:53:32 +0000

    Set up sonarcloud via actions

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,29 @@ module.exports = {
   testMatch: ['**/*.test.ts'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest'
-  }
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  projects: [
+    {
+      displayName: 'infrastructure',
+      testMatch: ['<rootDir>/test/infrastructure/**/*.test.ts'],
+      preset: 'ts-jest',
+      testEnvironment: 'node'
+    },
+    {
+      displayName: 'unit',
+      testMatch: ['<rootDir>/test/unit/**/*.test.ts'],
+      preset: 'ts-jest',
+      testEnvironment: 'node'
+    },
+    {
+      displayName: 'integration',
+      testMatch: ['<rootDir>/test/integration/**/*.test.ts'],
+      preset: 'ts-jest',
+      testEnvironment: 'node'
+    }
+  ],
+  coveragePathIgnorePatterns: [
+    '/node_modules/'
+  ],
 };
--- test/infrastructure/todo-app-project.test.ts
+++ test/infrastructure/todo-app-project.test.ts
@@ -1,6 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
 import { Template } from 'aws-cdk-lib/assertions';
-import * as TodoAppProject from '../lib/todo-app-stack';
+import * as TodoAppProject from '../../lib/todo-app-stack';
 
 test('S3 Bucket Created', () => {
   const app = new cdk.App();
